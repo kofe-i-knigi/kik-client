cf91e4832a90354159e0f7bc87c5f5ed18506378
Ksenia <Ksenia>
2016-05-18 23:51:40 +0500

    products

--- src/controllers/crud.js
+++ src/controllers/crud.js
@@ -2,12 +2,13 @@ export default class CrudCtrl {
   constructor(Resource, ngTableParams) {
     this.Resource = Resource;
     this.newEntity = new Resource();
-
+    this.filters = {}
     this.tableParams = new ngTableParams({
       page: 1,
       count: 20,
       sorting: {},
-      filter: this.filters,
+      filter : {},
+      // filter: this.filters,
       filterDelay: 1000
     }, {
       getData(params) {
