add47f39d94807d3a98405e5fe05ee3747524932
Stepan Shilin <sosloow@gmail.com>
2017-07-27 12:17:18 +0500

    change api root

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ var API_BASE = 'http://localhost/api';
 //var API_BASE = 'http://kofe-knigi.tk/api';
 
 if (process.env.NODE_ENV === 'production') {
-  API_BASE = 'http://kofe-knigi.tk/api';
+  API_BASE = 'http://kofe-knigi.ru/api';
 }
 
 export {API_BASE};
